6f1f72ba9337eaa5718dd7d8a6cae56cbb70c55c
Vladyslav Banasko <vlad.bona@gmail.com>
2021-02-15 17:47:33 -0500

    component added

--- Angular weather/AngularWeather/src/app/app.module.ts
+++ Angular weather/AngularWeather/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ContentComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

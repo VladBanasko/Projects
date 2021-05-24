da5830e4ea8ab7b32341d6898a7723f17d82ebe3
Vladyslav Banasko <vlad.bona@gmail.com>
2021-05-24 17:02:13 -0400

    Update HoursCountApp.py

--- testProjects/HoursCountApp.py
+++ testProjects/HoursCountApp.py
@@ -2,16 +2,19 @@
 # of app to implement it's logic on swift later)
 def countHours():
 
-	pay = 22
 
-	totalHours = 0
 
-	startHours = input("input time start")
-	print("you entered " + startHours)
-	finishHours = input("input fours finish")
+    pay = 22
 
-	totalHours = finishHours - startHours
+    totalHours = 0
 
-	total = totalHours * pay
+    startHours = int(input("input time start: "))
+    print("you entered " + startHours)
+    finishHours = int(input("input time finish: "))
+    print("you entered " + finishHours)
 
-	print("time added and for now sum is " + total)
+    totalHours = finishHours - startHours
+
+    total = totalHours * pay
+
+    print("time added and for now sum is " + total)

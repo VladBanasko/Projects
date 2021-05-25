32aa36b1616cb32f970c9875266871346dfb56e6
Vladyslav Banasko <vlad.bona@gmail.com>
2021-05-24 20:10:48 -0400

    Update HoursCountApp.py

--- testProjects/HoursCountApp.py
+++ testProjects/HoursCountApp.py
@@ -1,20 +1,42 @@
-# application that take start time and finish time of shift from user(test version 
+# application that take start time and finish time of shift from user(test version
 # of app to implement it's logic on swift later)
-def countHours():
+#def counthours():
 
+from datetime import datetime
+pay = 22
 
+totalhours = 0
 
-    pay = 22
+#startHours = int(input("input time start: "))
+#print("you entered " + str(startHours))
+#finishHours = int(input("input time finish: "))
+#print("you entered " + str(finishHours))
 
-    totalHours = 0
+#starttime =
+#starttime = str(input('Enter start time(yyyy-mm-dd hh:mm): '))
 
-    startHours = int(input("input time start: "))
-    print("you entered " + startHours)
-    finishHours = int(input("input time finish: "))
-    print("you entered " + finishHours)
+#start = datetime.strptime(starttime, "%Y-%m-%d %H:%M")
 
-    totalHours = finishHours - startHours
+#finishtime = str(input('Enter finish time(yyyy-mm-dd hh:mm): '))
 
-    total = totalHours * pay
+#finish = datetime.strptime(finishtime, "%Y-%m-%d %H:%M")
 
-    print("time added and for now sum is " + total)
+starttime = str(input('Enter start time( hh:mm): '))
+
+start = datetime.strptime(starttime, "%H:%M")
+
+finishtime = str(input('Enter finish time(yyyy-mm-dd hh:mm): '))
+
+finish = datetime.strptime(finishtime, "%Y-%m-%d %H:%M")
+
+timedifference = finish- start
+
+print(timedifference)
+
+#totalhours = finishHours - startHours
+
+#total = totalhours * pay
+total = float(timedifference) * pay
+
+print("time added and for now sum is " + str(total))
+
